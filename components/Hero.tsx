315e3e244df3dc0e1066c9b60762dd298971f321
unknown <ritamacharya2004@gmail.com>
2025-05-13 18:24:06 +0530

    updated hero-section part2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,12 +7,42 @@ export default function Hero(){
                 <div className="w-full md:w-1/2 text-left mt-10 md:mt-0 text-3xl font-bold tracking-tighter leading-8">Sundown is a multi-disciplinary <br />studio focused on creating <br />unique, end-to-end experiences <br />and environments.</div>
                 <div className="w-full md:w-1/2 text-right font-bold text-[100px] lg:text-[130px] leading-20 lg:leading-28 tracking-tight ">SPACES <br /> THAT <br />INSPIRE</div>
             </div>
+
+            {/**  video section  */}
             <div className="h-auto pb-20 rounded-3xl pt-15 w-full relative ">
-                <div className="absolute -top-25 -right-[2.5%] lg:-right-10 w-[70%] md:w-1/2 rounded-l-[90%]  h-[70%] blur-[15px] bg-[#FE330C] ">
-                    <div id="box" className="h-[35%] w-[40%] rounded-[100%] left-30 blur-[20px]"></div>
+                <div className="absolute -top-25 -right-[2.5%] lg:-right-10 w-[70%] md:w-1/2 rounded-bl-[90%] rounded-tl-[80%]  h-[70%] blur-[15px] bg-[#FE330C] ">
+                    <div id="box" className="h-[35%] w-[40%] rounded-[100%] left-30 bg-[#FE330C] blur-[20px]"></div>
                 </div>
                 <div className="h-auto w-full  rounded-3xl overflow-hidden relative ">
                     <video src="./video.mp4" autoPlay muted loop ></video>
+                </div>
+            </div>
+
+            {/**  moving text  */}
+            <div id="moving-text" className="flex text-[9vw] overflow-x-auto items-center justify-between gap-5 ">
+                <div className="flex items-center justify-between gap-5 content">
+                    <h1>CONTENT</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
+                    <h1>ENVIRONMENTS</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
+                    <h1>EXPERIENCES</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35x] rounded-[100%] ml-4 mr-4 "></div>
+                </div>
+                <div className="flex items-center justify-between gap-5 content">
+                    <h1>CONTENT</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
+                    <h1>ENVIRONMENTS</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
+                    <h1>EXPERIENCES</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
+                </div>
+                <div className="flex items-center justify-between gap-5 content">
+                    <h1>CONTENT</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
+                    <h1>ENVIRONMENTS</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
+                    <h1>EXPERIENCES</h1>
+                    <div className="bg-[#FE3E12] md:h-[70px] md:w-[70px] h-[35px] w-[35px] rounded-[100%] ml-4 mr-4 "></div>
                 </div>
             </div>
         </>
